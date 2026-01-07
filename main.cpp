339f78683e23e4c2eb2ac5500479d6e020bb08e6
Zeiron-cmd <timuvasilij@gmail.com>
2026-01-07 15:52:15 +0300

    Web client: auth integration and public access via ngrok

--- main.cpp
+++ main.cpp
@@ -2,10 +2,33 @@
 
 int main() {
     crow::SimpleApp app;
-
+    
     CROW_ROUTE(app, "/")([](){
         return "<h1>Hello Web Client</h1>";
     });
+
+    CROW_ROUTE(app, "/me")([](const crow::request& req){
+        auto cookie = req.get_header_value("Cookie");
+
+        if (cookie.find("session_token=") == std::string::npos) {
+            return crow::response(401, "Unauthorized");
+        }
+
+        return crow::response(200, "You are authorized");
+    });
+    CROW_ROUTE(app, "/logout")([](){
+        crow::response res("Logged out");
+        res.add_header("Set-Cookie", "session_token=; Max-Age=0");
+        return res;
+    });
+    CROW_ROUTE(app, "/login")([](){
+        crow::response res;
+        res.code = 302;
+        res.add_header("Location", "https://religiose-multinodular-jaqueline.ngrok-free.dev/docs#/Auth/github_oauth_url_auth_github_url_get");
+        return res;
+    });
+
+
 
     app.port(8080).multithreaded().run();
 }
